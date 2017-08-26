956465f59ad499539825c04236635fa72d517713
Johnny Dellaluna <johnnydellaluna@gmail.com>
2017-08-25 22:22:27 -0400

    ready to submit for homework
    Still have to tie up loose ends, including getting clearInterval to work, answers to register from user input, and results screen

--- Trivia Game/assets/javascript/app.js
+++ Trivia Game/assets/javascript/app.js
@@ -1,12 +1,41 @@
 window.onload = function() {
 	var time = 60;
     $("#displaytime").text("Time remaining: " + time);
-        var count = setTimeout(function(){
+        var count = setInterval(function(){
             time--;
             $("#displaytime").text("Time remaining: " + time)
 		}, 1000);
-    
-}
 
-// Program correct and incorrect answer lists.
-// Mark correct answers as such and write to results screen. Same for incorrect answers.
+		var clearInterval = stop.count
+
+        if (time === 0) {
+        	clearInterval(time);
+        }
+
+// Setting up countdown from 60. Stopping at 0.
+
+var questions = [];
+var answers = ["q1a", "q1b", "q1d", "q2a", "q2b", "q2c", "q3b", "q3c", "q3d", "q4a", "q4b", "q4c", "q5a", "q5c", "q5d"];
+var correctAnswers = ["q1c", "q2d", "q3a", "q4d", "q5b"];
+var right = 0;
+var wrong = 0;
+
+// Creating vars of right and all answers based on the ids of the radio buttons in HTML.
+
+    if ($('input[id]:checked').val() === correctAnswers[i]){
+
+        right++;
+
+    } else {
+
+        wrong++;
+    }
+
+    $('type:button').on("click", function () {
+        event.preventDefault();
+        $("body").html("Correct: " + right);
+    })
+
+// Updating user score on button click and creating new page with results.
+
+}
